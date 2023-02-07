3201ab81ec0a8459380819570523dc2d72690260
GodzoSioda <godzosioda@gmail.com>
2023-02-07 11:49:29 +0200

    Bigger tree

--- Tree.java
+++ Tree.java
@@ -1,9 +1,11 @@
 public class Tree {
           public static void main (String[] args) {
-		  System.out.println("   *   ");
-		  System.out.println("  ***  ");
-		  System.out.println(" ***** ");
-		  System.out.println("*******");
-		  System.out.print  ("   *   ");
+             System.out.println("    *    ");
+		  System.out.println("    *    ");
+		  System.out.println("   ***   ");
+		  System.out.println("  *****  ");
+		  System.out.println(" ******* ");
+		  System.out.println("*********");
+             System.out.print  ("   ***   ");
 		  }
     }
